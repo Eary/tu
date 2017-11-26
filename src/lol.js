0fbb876ec3f240725ba491d9321b4b00ed8c9e1a
dotos <yy.liu@foxmail.com>
2017-11-26 14:28:10 +0800

    upload

--- src/lol.js
+++ src/lol.js
@@ -2,59 +2,238 @@
  * LOL
  * Valine Comment System Data management platform
  * https://github.com/xCss
- */
-!(function(root,factory){
-    if(typeof define === 'function' && define.amd){
-        define(factory)
-    }else if(typeof exports === 'object' && module.exports){
-        module.exports = factory(root); // nodejs support
-        module.exports['default'] = module.exports; // es6 support
-    }else root.lol = factory(root);
-})(typeof window !== 'undefined' ? window : this,
-function(){
-    function LOL(opts){
-        var root = this;
-        root.Init(opts);
+*/
+
+import md5 from 'blueimp-md5';
+import marked from 'marked';
+import detect from './detect';
+
+function LOL(opts){
+    var root = this;
+    root.Init(opts);
+    return root;
+}
+
+LOL.prototype.Init = function(opts){
+    var root = this,
+        av = AV,
+        el = ({}).toString.call(opts.el) === "[object String]"?document.querySelector(opts.el):opts.el;
+
+    if(!av){
+        setTimeout(function(){
+            root.Init(opts);
+        },20);
+        return;
     }
+    av.init({
+        appId:opts.appId,
+        appKey:opts.appKey,
+        masterKey:opts.masterKey
+    });
+    av._useMasterKey = !0;
 
-    LOL.prototype.Init = function(opts){
-        var root = this,
-            av = AV,
-            el = ({}).toString.call(opts.el) === "[object String]"?document.querySelector(opts.el):opts.el;
+    root.v = av;
 
-        if(!av){
-            setTimeout(function(){
-                root.Init(opts);
-            },20);
-            return;
+    if(el){
+        
+        root.el = el;
+        root.el.classList.add('valine');
+        let eleHTML = `<div style="display:none;" class="vmark"></div></div><div class="vinfo" style="display:none;"><div class="vcount col"></div></div><ul class="vlist"></ul><div class="vempty" style="display:none;"></div><div class="vpage txt-center" ></div><div class="info"><div class="power txt-right">Powered By <a href="https://valine.js.org" target="_blank">Valine</a></div></div>`;
+        root.el.innerHTML = eleHTML;
+
+        // Empty Data
+        let vempty = root.el.querySelector('.vempty');
+        root.nodata = {
+            show(txt) {
+                vempty.innerHTML = txt || '还没有评论哦~';
+                vempty.setAttribute('style', 'display:block;');
+                return root;
+            },
+            hide() {
+                vempty.setAttribute('style', 'display:none;');
+                return root;
+            }
         }
 
-        av.init({
-            appId:opts.appId,
-            appKey:opts.appKey,
-            masterKey:opts.masterKey
-        });
+        // loading
+        let _spinner = document.createElement('li');
+        _spinner.setAttribute('class', 'vloading');
+        _spinner.innerHTML = `<div class="loading loading--double"></div>`;
+        // loading control
+        let _vlist = root.el.querySelector('.vlist');
+        root.loading = {
+            show(mt) {
+                let _vlis = _vlist.querySelectorAll('li');
+                if (mt) _vlist.insertBefore(_spinner, _vlis[0]);
+                else _vlist.appendChild(_spinner);
+                root.nodata.hide();
+                return root;
+            },
+            hide() {
+                let _loading = _vlist.querySelector('.vloading');
+                if (_loading) _vlist.removeChild(_loading)
+                _vlist.querySelectorAll('.vcard').length === 0 && root.nodata.show()
+                return root;
+            }
+        };
+    }
+    root.Bind();
+    return root;
+    //root.load()
+}
 
-        root.v = av;
-        root.el = el;
-
-        //root.load()
+LOL.prototype.Bind = function(){
+    var root = this;
+    var num = 1;
+    var query = function (n) {
+        n = n || num;
+        var size = 20;
+        var count = Number(root.el.querySelector('.vnum').innerText);
+        root.loading.show();
+        var cq = root.Q();
+        cq.limit(size);
+        cq.skip((n - 1) * size);
+        cq.find().then(rets => {
+            var len = rets.length;
+            for (var i = 0; i < len; i++) {
+                insertDom(rets[i], !0)
+            }
+            var _vpage = root.el.querySelector('.vpage');
+            _vpage.innerHTML = size * n < count ? `<button type="button" class="vmore vbtn">查看更多...</button>` : '';
+            var _vmore = _vpage.querySelector('.vmore');
+            if (_vmore) {
+                Event.on('click', _vmore, (e) => {
+                    _vpage.innerHTML = '';
+                    query(++num)
+                })
+            }
+            root.loading.hide();
+        }).catch(ex => {
+            console.log(ex)
+            root.loading.hide().ErrorHandler(ex.code)
+        })
     }
 
-    LOL.prototype.Bind = function(){
+    
+    root.Q().count().then(num => {
+        if (num > 0) {
+            root.el.querySelector('.vinfo').setAttribute('style', 'display:block;');
+            root.el.querySelector('.vcount').innerHTML = `<span class="vnum">${num}</span> 条评论`;
+            query();
+        } else {
+            root.loading.hide();
+        }
+    }).catch(ex => {
+        root.ErrorHandler(ex.code)
+    });
 
+
+    var insertDom = function(ret, mt) {
+        var _vcard = document.createElement('li');
+        _vcard.setAttribute('class', 'vcard');
+        _vcard.setAttribute('id', ret.id);
+        var _img = `<img class="vimg" src='https://gravatar.cat.net/avatar/${md5(ret.get('mail'))}'>`;
+        var ua = detect(ret.get('ua'));
+        var browser = `<span class="vsys">${ua.browser} ${ua.version}</span>`;
+        var os = `<span class="vsys">${ua.os} ${ua.osVersion}</span>`;
+        var path = `<span class="vsys">${ret.get('url')}</span>`;
+        var _nick = '';
+        var _t = ret.get('link') || (!!ret.get('mail') ? `mailto:${ret.get('mail')}` : '');
+        _nick = _t ? `<a class="vname" rel="nofollow" href="${_t}" target="_blank" >${ret.get("nick")}</a>` :`<span class="vname">${ret.get('nick')}</span>`;
+        _vcard.innerHTML = `${_img}<section><div class="vhead">${_nick} ${path}</div><div class="vcontent">${ret.get("comment")}</div><div class="vfooter"><span class="vtime">${dateFormat(ret.get("createdAt"))}</span><span rid='${ret.id}' at='@${ret.get('nick')}' mail='${ret.get('mail')}' class="del vat">删除</span><div></section>`;
+        var _vat = _vcard.querySelector('.vat');
+        var _as = _vcard.querySelectorAll('a');
+        for (var i = 0, len = _as.length; i < len; i++) {
+            var item = _as[i];
+            if (item && item.getAttribute('class') != 'at') {
+                item.setAttribute('target', '_blank');
+                item.setAttribute('rel', 'nofollow');
+            }
+        }
+        var _vlist = root.el.querySelector('.vlist');
+        var _vlis = _vlist.querySelectorAll('li');
+        if (mt) _vlist.appendChild(_vcard);
+        else _vlist.insertBefore(_vcard, _vlis[0]);
+        var _vcontent = _vcard.querySelector('.vcontent');
+        if (_vcontent) expandEvt(_vcontent);
+        if (_vat) bindDevEvt(_vat);
     }
 
-    LOL.prototype.Q = function(){
-        var root = this,
-            q = new root.v.Query('Comment');
-        q.descending('createdAt');
-        return q;
+    
+    // expand event
+    var expandEvt = function(el){
+        setTimeout(function () {
+            if (el.offsetHeight > 180) {
+                el.classList.add('expand');
+                Event.on('click', el, function(e){
+                    el.setAttribute('class', 'vcontent');
+                })
+            }
+        }, 20)
     }
 
-    function lolFactory(opts){
-        return new LOL(opts);
+    var bindDevEvt = function(el){
+        var id = el.getAttribute('rid')
+        Event.on('click',el,function(e){
+            if(confirm('确定删除该数据?')){
+                root.D(id).then(ret=>{
+                    console.log(ret)
+                }).catch(ex=>{
+                    console.log(ex)
+                })
+            }
+        })
     }
+}
 
-    return lolFactory;
-});
+LOL.prototype.Q = function(){
+    var root = this,
+        q = new root.v.Query('Comment');
+    q.descending('createdAt');
+    return q;
+}
+
+LOL.prototype.D = function(id){
+    var root = this;
+    var comment = root.v.Object.createWithoutData('Comment', id);
+    return comment.destroy();
+}
+
+var Event = {
+    on:function(type, el, handler, capture) {
+        if (el.addEventListener) el.addEventListener(type, handler, capture || false);
+        else if (el.attachEvent) el.attachEvent(`on${type}`, handler);
+        else el[`on${type}`] = handler;
+        return this;
+    },
+    off:function(type, el, handler, capture) {
+        if (el.removeEventListener) el.removeEventListener(type, handler, capture || false);
+        else if (el.detachEvent) el.detachEvent(`on${type}`, handler);
+        else el[`on${type}`] = null;
+        return this;
+    }
+}
+
+const dateFormat = (date) => {
+    var vDay = padWithZeros(date.getDate(), 2);
+    var vMonth = padWithZeros(date.getMonth() + 1, 2);
+    var vYear = padWithZeros(date.getFullYear(), 2);
+    var vHour = padWithZeros(date.getHours(), 2);
+    var vMinute = padWithZeros(date.getMinutes(), 2);
+    var vSecond = padWithZeros(date.getSeconds(), 2);
+    return `${vYear}-${vMonth}-${vDay} ${vHour}:${vMinute}:${vSecond}`;
+}
+
+var padWithZeros = function(vNumber, width){
+    var numAsString = vNumber.toString();
+    while (numAsString.length < width) {
+        numAsString = '0' + numAsString;
+    }
+    return numAsString;
+}
+
+function lolFactory(opts){
+    return new LOL(opts);
+}
+
+module.exports = lolFactory
--- webpack.config.js
+++ webpack.config.js
@@ -2,54 +2,76 @@ var webpack = require('webpack');
 var path = require('path');
 var autoprefixer = require('autoprefixer');
 
-var libraryName = 'lol';
-var env = process.env.WEBPACK_ENV;
+var libraryName = 'Valine';
 var ROOT_PATH = path.resolve(__dirname);
 var APP_PATH = path.resolve(ROOT_PATH, 'src');
 var BUILD_PATH = path.resolve(ROOT_PATH, 'dist');
 
 var plugins = [];
-if (env !== 'dev') {
-    plugins.push(
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: false,
-                drop_console: true
-            },
-            sourceMap: false
-        })
-    );
-    plugins.push(new webpack.LoaderOptionsPlugin({minimize:true}));
-}
 
-module.exports = {
-    entry: './src/' + libraryName + '.js',
+module.exports = env => {
+    var dev = env && env.dev || false
+    if (!dev) {
+        plugins.push(
+            new webpack.optimize.UglifyJsPlugin({
+                // 最紧凑的输出
+                beautify: false,
+                // 删除所有的注释
+                comments: false,
+                sourceMap: false,
+                compress: {
+                    // 在UglifyJs删除没有用到的代码时不输出警告
+                    warnings: false,
+                    // 删除所有的 `console` 语句
+                    drop_console: true,
+                }
+            })
+        );
+        plugins.push(new webpack.LoaderOptionsPlugin({ minimize: true }));
+    } else {
+        plugins.push(new webpack.NamedModulesPlugin())
+        plugins.push(new webpack.HotModuleReplacementPlugin())
+    }
+    return {
+        entry: {
+            lol:['./src/lol.scss','./src/lol.js']
+            //Valine: ['./src/lol.scss', './src/lol.js']
+            //'Valine.pure': './src/Valine.js',
+            //detect:'./src/detect.js',
+            //escape:'./src/escape.js'
+        },
+        output: {
+            path: BUILD_PATH,
+            filename: '[name].min.js',
+            library: '[name]',
+            libraryTarget: 'umd',
+            umdNamedDefine: true
+        },
 
-    output: {
-        path: BUILD_PATH,
-        filename: libraryName + '.min.js',
-        library: libraryName,
-        libraryTarget: 'umd',
-        umdNamedDefine: true
-    },
+        devtool: 'cheap-module-source-map',
 
-    devtool: 'source-map',
+        devServer: {
+            hot: true,
+            port: 8088,
+            inline: true,
+            progress: true,
+            host: '0.0.0.0',
+            publicPath: "/dist/",
+            historyApiFallback: true,
+            stats: 'errors-only', //只在发生错误时输出
+            overlay: { //当有编译错误或者警告的时候显示一个全屏overlay
+                errors: true,
+                warnings: true,
+            }
+        },
 
-    devServer: {
-        publicPath: "/dist/",
-        port: 8088
-    },
-
-    module: {
-        rules: [{
+        module: {
+            rules: [{
                 test: /\.js$/,
                 loader: 'babel-loader',
                 include: [APP_PATH],
-                exclude: /node_modules/,
-                query: {
-                    presets: ['env']
-                }
-            },{
+                exclude: /node_modules/
+            }, {
                 test: /\.scss$/,
                 use: [
                     'style-loader',
@@ -58,19 +80,21 @@ module.exports = {
                     'sass-loader'
                 ],
                 include: APP_PATH
-            },{ 
-                test: /\.css$/, 
+            }, {
+                test: /\.css$/,
                 use: [
                     'style-loader',
                     'css-loader',
                     'postcss-loader'
                 ]
-            },{
-                test: /\.(png|jpg)$/,
-                loader: 'url-loader?limit=40000'
+            }, {
+                test: /\.(png|jpg|gif)$/,
+                use: ['url-loader?limit=8192']
+                //loader: 'url-loader?limit=40000'
             }
-        ]
-    },
+            ]
+        },
 
-    plugins: plugins
+        plugins: plugins
+    }
 };
